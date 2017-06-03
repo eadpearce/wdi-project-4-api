526fed3ee987558a6f098baf48a448433f4bf183
eadpearce <e.pearce@tutanota.com>
2017-06-03 11:31:58 +0100

    Tags get converted to lowercase before being saved

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -16,6 +16,9 @@ class TagsController < ApplicationController
   # POST /tags
   def create
     @tag = Tag.new(tag_params)
+    # change to lowercase to avoid duplicates with odd cases
+    @tag.name.downcase!
+    # avoid creating duplicates with the same name 
     @tag = Tag.find_or_create_by(name: @tag.name)
 
     if @tag.save
