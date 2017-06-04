4b99d3d866e10009d77931c9d094f86f69a46377
eadpearce <e.pearce@tutanota.com>
2017-06-04 16:20:04 +0100

    Added attribute to prompts whether they are filled or unfilled

--- app/serializers/prompt_serializer.rb
+++ app/serializers/prompt_serializer.rb
@@ -1,5 +1,5 @@
 class PromptSerializer < ActiveModel::Serializer
-  attributes :id, :title, :body, :date_created, :created_at
+  attributes :id, :title, :body, :date_created, :created_at, :filled, :unfilled
   has_one :user
   has_many :tags
   has_many :fills
@@ -9,5 +9,20 @@ class PromptSerializer < ActiveModel::Serializer
     date = object.created_at
     formatted_date = date.strftime('%c')
     formatted_date
+  end
+
+  def filled
+    if object.fills.length != 0
+      filled = true
+    else filled = false
+    end
+    filled
+  end
+  def unfilled
+    if object.fills.length != 0
+      unfilled = false
+    else unfilled = true
+    end
+    unfilled
   end
 end
