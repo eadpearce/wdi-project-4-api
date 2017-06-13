12de75b4c484c6025ef1afd5216a74edd2b09170
eadpearce <e.pearce@tutanota.com>
2017-06-13 23:04:47 +0100

    Auth not working on heroku.

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ require "action_cable/engine"
 # require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
-require "/lib/auth.rb"
+require "auth.rb"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
